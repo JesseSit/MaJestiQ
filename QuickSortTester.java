766b95ba224b629ec1f25d8c5227b5ad51a6c456
JesseSit <jsit@stuy.edu>
2017-03-09 10:14:54 -0500

    add test

--- QuickSortTester.java
+++ QuickSortTester.java
@@ -1,3 +1,10 @@
+/*
+Team Name: MaJestiQ
+Team Members: Matteo 
+*/
+/*
+  This method takes an int value l and builds an array of size l. Then you run qsort to get the runtime for one execution and add it to a counter. Reshuffle and repeat for 10000 times. Divide the counter by 10000 to get the average runtime.
+*/
 public class QuickSortTester{
     
     public static long test(int l){
@@ -12,6 +19,7 @@ public class QuickSortTester{
 	return counter / 10000;
     }
     public static void main(String[] args){
+	System.out.println("Array length 1: " + test(1));
 	System.out.println("Array length 10: " + test(10));
 	System.out.println("Array length 50: " + test(50));
 	System.out.println("Array length 100: " + test(100));
@@ -19,5 +27,6 @@ public class QuickSortTester{
 	System.out.println("Array length 500: " + test(500));
 	System.out.println("Array length 750: " + test(750));
 	System.out.println("Array length 1000: " + test(1000));
+	System.out.println("Array length 10000: " + test(10000));
     }
 }
